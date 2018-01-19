78acf3d03206194b5fa156ccd0d1ef7870c35729
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-19 17:31:14 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,11 @@ def monopoly_with_third_tier
 		monopoly = {
 			:railroads => {
 				:pieces=>4,
-				:names=>{},
+				:names=>{
+					:reading_railroad=>{},
+					:pennsylvania_railroad=>{},
+					:b_and_o_railroad=>{}
+				},
 				:rent_in_dollars=>{
 					:one_piece_owned=>25,
 					:two_pieces_owned=>50,
