5cf87f0a1962d931c5522ebc03064eba52cc7024
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-19 17:32:49 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,24 +25,23 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	def monopoly_with_second_tier
-		monopoly = {
-			:railroads => {
-				:pieces=>4,
-				:names=>{
-					:reading_railroad=>{},
-					:pennsylvania_railroad=>{},
-					:b_and_o_railroad=>{},
-					:shortline=>{}
-				},
-				:rent_in_dollars=>{
-					:one_piece_owned=>25,
-					:two_pieces_owned=>50,
-					:three_pieces_owned=>100,
-					:four_pieces_owned=>200
-				}
+	monopoly = {
+		:railroads => {
+			:pieces=>4,
+			:names=>{
+				:reading_railroad=>{},
+				:pennsylvania_railroad=>{},
+				:b_and_o_railroad=>{},
+				:shortline=>{}
+			},
+			:rent_in_dollars=>{
+				:one_piece_owned=>25,
+				:two_pieces_owned=>50,
+				:three_pieces_owned=>100,
+				:four_pieces_owned=>200
 			}
 		}
+	}
 
 end
 
